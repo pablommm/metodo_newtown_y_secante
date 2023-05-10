e16a86cb87c0e8bf7fffecfd8f645c7e0756327b
pablommm <murgiapablo@gmail.com>
2023-05-09 23:03:38 -0300

    agrego funciones para testar como las grafica

--- test_matplotlib.py
+++ test_matplotlib.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Crea un conjunto de datos x
-x = np.linspace(0, 2*np.pi, 100)
+x = np.linspace(-10, 2*np.pi, 100)
 
 # Crea un conjunto de datos y correspondiente para la función seno
-y = np.sin(x)
-
+#y = np.sin(x)
+#y = (x * x) - 2
+y = x**3 + 4*(x**2) -10
 # Crea la gráfica usando Matplotlib
 plt.plot(x, y)
 
